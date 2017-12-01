c05725278f372a7019eac32cbb7c1572b907fe45
SomebodyLuo <luoyouren26@163.com>
2017-12-01 19:28:24 +0800

    add more;

--- app/src/main/assets/Res/test.fs
+++ app/src/main/assets/Res/test.fs
@@ -9,5 +9,5 @@ varying vec2 V_Texcoord;
 
 void main()
 {
-    gl_FragColor = V_Color * texture2D(U_Texture, V_Texcoord);
+    gl_FragColor = V_Color * texture2D(U_Texture, V_Texcoord) * texture2D(U_Texture2, V_Texcoord);
 }
